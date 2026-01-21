8c751bfe780264a42b67b45bfbbddb75f29a617f
Rodrigo <rodrigo@example.com>
2026-01-21 16:27:35 +0100

    Refactor memory module

--- hw/RTL/memory/mem.sv
+++ hw/RTL/memory/mem.sv
@@ -21,19 +21,15 @@ module bram #(
 
   (* ram_style="block" *) logic [DATA_WIDTH-1:0] mem [0:(1<<ADDR_WIDTH)-1];
 
-  // Port A
   always_ff @(posedge clk) begin
     if (en_a) begin
       if (we_a) begin
         for (int i = 0; i < DATA_WIDTH/8; i++)
           if (wstrb_a[i]) mem[addr_a][8*i +: 8] <= din_a[8*i +: 8];
       end
-      dout_a <= mem[addr_a]; // 1-cycle sync read
+      dout_a <= mem[addr_a];
     end
-  end
 
-  // Port B
-  always_ff @(posedge clk) begin
     if (en_b) begin
       if (we_b) begin
         for (int i = 0; i < DATA_WIDTH/8; i++)
